70c03eb71948a503b59a5b91f55e4082390a1d9b
lyashevska <olga@herenstraat.nl>
2022-04-03 20:31:48 +0100

    implemented from baeldung

--- src/main/java/nl/lyashevska/mmtspringboot/config/HibernateSearchConfiguration.java
+++ src/main/java/nl/lyashevska/mmtspringboot/config/HibernateSearchConfiguration.java
@@ -1,32 +1,79 @@
+/**
+ * https://www.baeldung.com/hibernate-search
+ * */
+
 package nl.lyashevska.mmtspringboot.config;
 
-import nl.lyashevska.mmtspringboot.hibernatesearch.HibernateSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
-@EnableAutoConfiguration
-//@EnableTransactionManagement
+@EnableTransactionManagement
 @Configuration
+//@PropertySource({ "classpath:persistence-h2.properties" })
+@EnableJpaRepositories(basePackages = { "nl.lyashevska.mmtspringboot.hibernatesearch" })
+@ComponentScan({ "nl.lyashevska.mmtspringboot.hibernatesearch" })
+
 public class HibernateSearchConfiguration {
 
     @Autowired
-    private EntityManager bentityManager;
+    private Environment env;
 
-    public HibernateSearchConfiguration(EntityManager bentityManager) {
-        this.bentityManager = bentityManager;
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+//        em.setDataSource(dataSource());
+        em.setPackagesToScan(new String[] { "nl.lyashevska.mmtspringboot.model" });
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+//        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+
+//    @Bean
+//    public DataSource dataSource() {
+//        final BasicDataSource dataSource = new BasicDataSource();
+//        dataSource.setDriverClassName(Preconditions.checkNotNull(env.getProperty("jdbc.driverClassName")));
+//        dataSource.setUrl(Preconditions.checkNotNull(env.getProperty("jdbc.url")));
+//        dataSource.setUsername(env.getProperty("jdbc.user"));
+//        dataSource.setPassword(env.getProperty("jdbc.pass"));
+//
+//        return dataSource;
+//    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
     }
 
     @Bean
-    HibernateSearchService hibernateSearchService() {
-        HibernateSearchService hibernateSearchService = new HibernateSearchService(bentityManager);
-        hibernateSearchService.initializeHibernateSearch();
-        return hibernateSearchService;
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
     }
-
-
+//
+//    Properties additionalProperties() {
+//        Properties properties = new Properties();
+//        properties.setProperty("hibernate.hbm2ddl.auto", Preconditions.checkNotNull(env.getProperty("hibernate.hbm2ddl.auto")));
+//        properties.setProperty("hibernate.dialect", Preconditions.checkNotNull(env.getProperty("hibernate.dialect")));
+//        return properties;
+//    }
 }
